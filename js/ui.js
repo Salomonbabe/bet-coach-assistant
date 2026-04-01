eeb22c092ce697153e788bf2a66dd629c88de54a
Salomon <pierrebabe89@gmail.com>
2026-04-01 18:33:03 +0000

    test render

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,10 @@
-function renderLogin() {
-  document.getElementById("app").innerHTML = `
-    <h2>Connexion</h2>
-    
-    <input id="email" placeholder="Email" /><br/><br/>
-    <input id="password" type="password" placeholder="Mot de passe" /><br/><br/>
-    
-    <button onclick="login()">Se connecter</button>
-    <button onclick="signup()">Créer un compte</button>
-  `;
+function renderDashboard() {
+  document.getElementById("app").innerHTML = "<h1>Dashboard OK</h1>";
 }
+
+function renderLogin() {
+  document.getElementById("app").innerHTML = "<h1>Login OK</h1>";
+}
+
 window.renderDashboard = renderDashboard;
 window.renderLogin = renderLogin;
